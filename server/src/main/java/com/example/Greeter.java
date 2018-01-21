fbd33709fc1bfc709bcb706219c3a4bf465567bf
trisha751 <trisha751@gmail.com>
2018-01-21 11:39:41 +0530

    ee

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,14 @@ public class Greeter {
 
   }
 
-  public abstract String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
+
+/** {@inheritDoc} */
+
+public int checkReturnTag(final int aTagIndex,
+                          JavadocTag[] aTags,
+                          int aLineNo)
+                          
